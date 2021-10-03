e2e598caeb1a03bcd7cc5173b057e5ff3262ec5b
Emerson Silva <emerson_jdss@hotmail.com>
2021-10-03 12:15:32 -0300

    config routes and initial route

---
+++ src/screens/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+// import { Container } from './styles';
+
+const Login = () => (
+  <>
+    <SafeAreaView>
+      <View>
+        <Text>
+          Emerson
+        </Text>
+      </View>
+    </SafeAreaView>
+  </>
+)
+
+
+export default Login;
